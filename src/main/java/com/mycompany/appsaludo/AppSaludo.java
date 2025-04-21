ddcd09a0c3b0297d50a2ec227128b654ace67dff
aulamovil <aulamovil@AV2AIOE08.etitc.local>
2025-04-21 15:13:28 -0500

    Diseño

---
+++ src/main/java/com/mycompany/appsaludo/AppSaludo.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appsaludo;
+
+public class AppSaludo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
